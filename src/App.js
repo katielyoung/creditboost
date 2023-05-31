c3adf84ff4f7a0f269e58823198bd998a193eab2
Katie Young <katelyn.l.young.96@gmail.com>
2023-05-31 03:02:10 -0700

    update home page

--- src/App.js
+++ src/App.js
@@ -1,28 +1,28 @@
+import React from "react";
+import "./App.css";
+import Navbar from "./components/Navbar";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Home from "./pages";
+import Profile from "./pages/profile";
+import LoanManage from "./pages/loan_manage";
+import SignUp from "./pages/signup";
+import Login from "./pages/login";
 
-import React from 'react';
-import './App.css';
-import Navbar from './components/Navbar';
-import { BrowserRouter as Router, Routes, Route }
-    from 'react-router-dom';
-import Home from './pages';
-import Profile from './pages/profile';
-import LoanManage from './pages/loan_manage';
-import SignUp from './pages/signup';
-import Login from './pages/login';
- 
 function App() {
-    return (
-        <Router>
-            <Navbar />
-            <Routes>
-                <Route exact path='/' element={<Home />} />
-                <Route path='/profile' element={<Profile />} />
-                <Route path='/loan_manage' element={<LoanManage />} />
-                <Route path='/signup' element={<SignUp />} />
-                <Route path='/login' element={<Login />} />
-            </Routes>
-        </Router>
-    );
+  return (
+    <div class = "homepage">
+      <Router>
+        <Navbar />
+        <Routes>
+          <Route exact path="/" element={<Home />} />
+          <Route path="/profile" element={<Profile />} />
+          <Route path="/loan_manage" element={<LoanManage />} />
+          <Route path="/signup" element={<SignUp />} />
+          <Route path="/login" element={<Login />} />
+        </Routes>
+      </Router>
+    </div>
+  );
 }
- 
+
 export default App;
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,14 +1,32 @@
 import React from "react";
+import styled from "styled-components";
+import home_pic from "../imgs/credit_score_boost.png";
+
+const Title = styled.h1`
+  font-size: 1.5em;
+  text-align: center;
+  color: #ffffff;
+`;
+
+const About = styled.p`
+  font-size: 1em;
+  text-align: center;
+  color: #ffffff;
+`;
 
 const Home = () => {
   return (
-    <div>
-      <h1>Build your credit score!</h1>
-      <p>
+    <div class="container">
+      <Title>Build your credit score!</Title>
+      <About>
         Unlock your financial potential with CreditBoost - the key to a brighter
-        future! Elevate your credit score and soar towards limitless
-        possibilities, all with the ease and security you deserve.
-      </p>
+        future!
+      </About>
+      <About>
+        Elevate your credit score and soar towards limitless possibilities, all
+        with the ease and security you deserve.
+      </About>
+      <img src={home_pic} alt="HomePic" />
     </div>
   );
 };
